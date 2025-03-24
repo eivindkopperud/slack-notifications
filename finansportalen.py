f781171f9017ef2b15c7e092caedbeaa6d0559a8
Håkon Verås <hakon.veras@storebrand.no>
2025-03-24 21:35:08 +0100

    refactor: generalize code for seb and finansportalen

--- finansportalen.py
+++ finansportalen.py
@@ -4,22 +4,33 @@ def process_data(data):
     processed_data = []
     for row in data['data']:
         try:
-            provider = row['boliglan']['leverandorVisningsnavn']
             rate = row['effektivRente'].replace(",",".")
-            processed_data.append({'provider': provider, 'rate': rate})
+            processed_data.append(rate)
         except:
             print("Failed to process row: {}".format(row))
             pass
     return processed_data
 
 def sort_data(data):
-    sorted_data = sorted(data, key=lambda k: k['rate'], reverse=True)
-    return sorted_data[:5]
+    return sorted(data, reverse=False)
 
 def get_latest_value_finansportalen():
     response = requests.get("https://www.finansportalen.no/services/kalkulator/boliglan?alderstilbudAr=45&fastrente_6_10=ja&kalkulatortype=laan&kjopesum=3000000&laan_type=bolig&lanebelop=1500000&lopetidtermin_value=30&nasjonalt=ja&neiforutsettermedlemskap=ja&rente=fastrente_rente&rentetakIgnore=ja&standardlan=ja&visProduktpakker=ja&visUtenProduktpakker=ja")
     data = response.json()
     processed_data = process_data(data)
-    sorted_data = sorted(processed_data, key=lambda k: k['rate'], reverse=False)
+    sorted_data = sort_data(processed_data)
 
-    return sorted_data[0:5]
+    return sorted_data[:5]
+
+def get_payload(old_value, new_value):
+    sum_old = sum(map(float, old_value))
+    sum_new = sum(map(float, new_value))
+    if sum_new > sum_old:
+        payload = {
+            "text": f":chart_with_upwards_trend: *Finansportalen har endret seg*\nGammel verdi: {old_value}\nNy verdi: {new_value}"
+        }
+    else:
+        payload = {
+            "text": f":chart_with_downwards_trend: *Finansportalen har endret seg*\nGammel verdi: {old_value}\nNy verdi: {new_value}"
+        }
+    return payload
--- main.py
+++ main.py
@@ -4,31 +4,23 @@ import json
 import datetime
 
 import finansportalen
+import seb
 
-gist_url = "https://api.github.com/gists/4d17720b7a328e065b73e08780fa946f"
+gist_url = "https://api.github.com/gists/52a8f1d35abbefdeae6a57071cee6512"
 gist_token = os.getenv("GIST_TOKEN")
 slack_webhook_url = os.getenv("SLACK_WEBHOOK_URL")
 
-def get_latest_value():
-    response = requests.get("https://sebgroup.com/ssc/trading/fx-rates-bff/api/rates/swap?currency=NOK")
-    data = response.json()
-    for row in data['rows']:
-        maturity = row['data'][0]['value']
-        value = row['data'][1]['value']
-        decimals = row['data'][1]['decimals']
-        diff = row['data'][2]['value']
-        if maturity == "10 Yr" and decimals == 2 and diff == 0.0:
-            return value
-    return None
 
 def get_gist_data(filename):
     headers = {"Authorization": f"token {gist_token}"}
     response = requests.get(gist_url, headers=headers)
+    print("get_gist_data", response.json())
     if response.status_code == 200:
         content = json.loads(response.json()['files'][filename]['content'])
-        
+
         return content["last_value"], content["last_updated"]
     return None, None
+
 
 def update_gist(new_value, filename):
     headers = {"Authorization": f"token {gist_token}"}
@@ -42,38 +34,43 @@ def update_gist(new_value, filename):
             }
         }
     }
-    requests.patch(gist_url, headers=headers, json=new_data)
+    response = requests.patch(gist_url, headers=headers, json=new_data)
+    print("update_gist", response.json())
 
-def send_slack_notification(new_value, old_value):
-    payload = {
-        "text": f":rotating_light: *SWAP Rate har endret seg*\nGammel verdi: {old_value}\nNy verdi: {new_value}"
-    }
-    requests.post(slack_webhook_url, json=payload)
 
-def main():
-    latest_value = get_latest_value()
-    latest_value_finansportalen = finansportalen.get_latest_value_finansportalen()
+def send_slack_notification(new_value, old_value, source):
+    if source == "finansportalen":
+        payload = finansportalen.get_payload(old_value, new_value)
+        requests.post(slack_webhook_url, json=payload)
+    elif source == "seb":
+        payload = seb.get_payload(old_value, new_value)
+        requests.post(slack_webhook_url, json=payload)
+    else:
+        print("Invalid source: {}", source)
 
-    if not latest_value:
-        print("Fant ingen gyldig verdi for 10 Yr")
+
+def fetch_and_update_data(source):
+    if source == "finansportalen":
+        latest_value = finansportalen.get_latest_value_finansportalen()
+    elif source == "seb":
+        latest_value = seb.get_latest_value_seb()
+    else:
+        print(f"Fant ingen gyldige verdier for {source}")
         return
 
-    last_value, _ = get_gist_data("rates.txt")
-    last_value_finansportalen, _ = get_gist_data("rates_finansportalen.txt")
+    last_value, _ = get_gist_data(f"rates_{source}.txt")
 
     if str(latest_value) != str(last_value):
-        print(f"Ny verdi funnet: endret fra {last_value} til {latest_value}")
-        update_gist(latest_value, "rates.txt")
-        send_slack_notification(latest_value, last_value)
+        print(f"Ny verdi funnet: endret fra {latest_value} til {latest_value}")
+        update_gist(latest_value, f"rates_{source}.txt", )
+        send_slack_notification(latest_value, last_value, source)
     else:
         print("Ingen endringer funnet.")
 
-    if latest_value_finansportalen != last_value_finansportalen:
-        print(f"Endring på Finansportalen: endret fra {last_value} til {latest_value}")
-        update_gist(latest_value, "rates_finansportalen.txt")
-        send_slack_notification(latest_value_finansportalen, last_value_finansportalen)
-    else:
-        print("Ingen endringer funnet.")
+
+def main():
+    fetch_and_update_data("seb")
+    fetch_and_update_data("finansportalen")
 
 
 if __name__ == "__main__":
